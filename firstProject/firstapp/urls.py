efe85d8911ba585a8ef0512fd50913074bf74739
neesraut <neesraut1@gmail.com>
2020-12-08 16:53:29 +1100

    form in django- usercreationform

--- firstProject/firstapp/urls.py
+++ firstProject/firstapp/urls.py
@@ -1,9 +1,21 @@
 
 from django.urls import path, include
-from firstapp.views import view1,view2,GetUsers
+from firstapp.views import(
+    view1,
+    view2,
+    GetUsers,
+    displayUsers,
+    signUp,
+    success
+)
 
 urlpatterns = [
     path('a/', view1.as_view(), name='view1'),
     path('b/', view2.as_view(), name='view2'),
     path('users/', GetUsers.as_view(), name='get-users'),
+
+    path('users1/', displayUsers, name='display-users'),
+    path('signupUsers/',signUp, name = 'signup-user'),
+    path('success/',success, name = 'success')
 ]
+
--- firstProject/firstapp/views.py
+++ firstProject/firstapp/views.py
@@ -5,6 +5,9 @@ from django.http import HttpResponse
 from django.views import View
 from  firstapp.models import Users
 
+##formcontent
+from django.contrib.auth.forms import UserCreationForm
+
 from django.shortcuts import render, get_object_or_404, redirect
 class view1(View):
     def get(self, request, *args, **kwargs):
@@ -15,10 +18,40 @@ class view2(View):
         return HttpResponse('This is my b view')
 
 class GetUsers(View):
-
-
     def get(self, request, *args, **kwargs):
         
         user_list = Users.objects.all().values()
-        print(user_list)
         return HttpResponse(user_list)
+
+##simpler form of views
+def displayUsers(request):
+    users_list = Users.objects.all().values()
+    template = 'home.html'
+    content = {
+        'users' : users_list
+
+
+    }
+    return render(request,template,content)
+
+def signUp(request):
+    print("yayayyayayya")
+    template = "signUp.html"
+
+    if request =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('success')
+    else:
+        form = UserCreationForm()
+    content={
+        'form' : form
+    }
+    
+
+    return render(request, template, content)
+
+def success(request):
+    template = 'success.html'
+    return render(request,template)
